4237486bdd9ab0e25b0c4c8c0d42a0443d0038dc
Max Clayton Clowes <maxcc@me.com>
2019-07-22 21:49:09 +0100

    [feat] wip proportional

--- insta_palette.py
+++ insta_palette.py
@@ -3,14 +3,31 @@
 import os, datetime, re, random, numpy, operator, cv2, colorsys
 from sklearn.cluster import KMeans
 from PIL import Image, ImageDraw
+from collections import Counter
 
-# settings
-DEBUG = False
+def debug(input):
+  print(input)
+
+# Settings
+DEBUG = True
+SAMPLE_IMAGE_X = 600
+SAMPLE_IMAGE_Y = 400
+
+# Image settings
 COLOR_BG = True
-PALETTE_HEIGHT = 168
-PALETTE_SIZE = 5
+PALETTE_SIZE = 7
+
+# Dimensions
 MARGIN = 40
-OFFSET = 208
+CANVASS_SIZE = 1080
+CANVAS_INNER = CANVASS_SIZE - (MARGIN * 2)
+
+PALETTE_HEIGHT = (CANVAS_INNER - (MARGIN * (PALETTE_SIZE - 1))) / PALETTE_SIZE
+PALETTE_OFFSET = PALETTE_HEIGHT + MARGIN
+PALETTE_INNER = 872
+PALETTE_OUTER = CANVASS_SIZE - MARGIN
+
+AVAILABLE_SPACE = CANVAS_INNER - (MARGIN * (PALETTE_SIZE - 1))
 
 def getBrightness(color):
   r, g, b = color
@@ -18,31 +35,48 @@ def getBrightness(color):
   return v
 
 def sortColors(colors):
-  if(DEBUG):
-    print(colors)
-    print(sorted(colors, key=lambda color: getBrightness(color), reverse=True))
+  debug(colors)
+  debug(sorted(colors, key=lambda color: getBrightness(color), reverse=True))
   # sort by brightness
   return sorted(colors, key=lambda color: getBrightness(color), reverse=True)
 
 def extractColors(image, n):
+  debug('> Extracting colors...')
+
+  # shrink image for efficiency
+  modified_image = cv2.resize(image, (SAMPLE_IMAGE_X, SAMPLE_IMAGE_Y), interpolation = cv2.INTER_AREA)
+  #modified_image = image
+
   # reshape image into a list of pixels
-  px_list = image.reshape((image.shape[0] * image.shape[1], 3))
+  px_list = modified_image.reshape((modified_image.shape[0] * modified_image.shape[1], 3))
 
   # run kmeans function
-  clt = KMeans(n_clusters = n)
-  clt.fit(px_list)
+  cluster = KMeans(n_clusters = n)
 
-  # get list of cluster colors
-  clus_colors = clt.cluster_centers_.astype("uint8").tolist()
-  clus_colors = sortColors(clus_colors)
+  debug('> Finding colors...')
 
-  return clus_colors
+  # find colors
+  fit = cluster.fit(px_list)
+  cluster_colors = fit.cluster_centers_.astype("uint8").tolist()
+
+  debug('> Finding color frequencies...')
+
+  # count colour frequencies
+  labels = cluster.fit_predict(px_list)
+  cluster_color_counts = Counter(labels)
+
+  for i in range(0, len(cluster_colors)):
+    cluster_colors[i] = [cluster_colors[i], cluster_color_counts[i]]
+
+  debug('> Colors exracted...')
+
+  return cluster_colors
 
 def createBackgroundImage(colorPalette):
   r, g, b = [255, 255, 255]
   if(COLOR_BG==True):
-    r, g, b = colorPalette[0]
-  return Image.new("RGB", (1080,1080),(r, g, b))
+    r, g, b = colorPalette[0][0]
+  return Image.new("RGB", (CANVASS_SIZE, CANVASS_SIZE),(r, g, b))
 
 def convertImages(input_files):
   # for every image in file list
@@ -61,23 +95,23 @@ def convertImages(input_files):
     old_y = old_size[1]
 
     # scale the image to have a 40 px buffer whether landscape or portrait
-    scale = old_x/float(1000)
-    small_y = int(old_y/scale)
+    scale = old_x / float(CANVAS_INNER)
+    small_y = int(old_y / scale)
 
     # if y axis is too big to provide enough room underneath for blur
     # set up image for portrait 'mode'
-    if small_y > (960-PALETTE_HEIGHT):
-      new_scale = old_y/float(1000)
-      small_im = old_im.resize((int(old_x/new_scale),1000), Image.ANTIALIAS)
+    if small_y > (CANVASS_SIZE - (MARGIN * 4) - PALETTE_HEIGHT):
+      new_scale = old_y / float(CANVAS_INNER)
+      small_im = old_im.resize((int(old_x / new_scale), CANVAS_INNER), Image.ANTIALIAS)
       aspect = "portrait"
       # if there won't be enough room to right of image for portrait mode blur
       # escape and provide warning message
-      if small_im.size[0] > (960-PALETTE_HEIGHT):
+      if small_im.size[0] > (960 - PALETTE_HEIGHT):
         print("ya gonna have to crop this bad boi")
         break
     # else we're gonna be working in landscape
     else:
-      small_im = old_im.resize((1000,small_y), Image.ANTIALIAS)
+      small_im = old_im.resize((CANVAS_INNER, small_y), Image.ANTIALIAS)
       aspect = "landscape"
 
     # get small_im into a cv2 compatible format
@@ -85,28 +119,38 @@ def convertImages(input_files):
 
     # calculate color palette
     colorPalette = extractColors(image, PALETTE_SIZE)
-
+    debug(colorPalette)
     # create background image
     new_im = createBackgroundImage(colorPalette)
 
     # paste small_im onto background new_im
-    new_im.paste(small_im,(MARGIN, MARGIN))
+    new_im.paste(small_im, (MARGIN, MARGIN))
     draw = ImageDraw.Draw(new_im)
 
-    # draw palette squares and fill with cluster colors
-    if aspect == "landscape":
-      for j in range(0, PALETTE_SIZE):
-        left = MARGIN + (OFFSET * j)
-        right = OFFSET * (j + 1)
-        draw.rectangle([left, 872, right, 1040], fill=tuple(colorPalette[j]), outline=None)
-    else:
-      for j in range(0, PALETTE_SIZE):
-        top = MARGIN + (OFFSET * j)
-        bottom = OFFSET * (j + 1)
-        draw.rectangle([872, top, 1040, bottom], fill=tuple(colorPalette[j]), outline=None)
+    debug('> Outputting...')
 
-    # get modified date of original photo - this allows the easy sorting of the
-    # output files by age of the original
+    # draw palette squares and fill with cluster colors
+    position_buffer = MARGIN
+
+    for j in range(0, PALETTE_SIZE):
+      percentage = colorPalette[j][1] / (SAMPLE_IMAGE_X * SAMPLE_IMAGE_Y)
+      color_width = percentage * AVAILABLE_SPACE
+
+      left = position_buffer
+      right = position_buffer + color_width
+
+      position_buffer = position_buffer + color_width + MARGIN
+
+      #left = MARGIN + (PALETTE_OFFSET * j)
+      #right = PALETTE_OFFSET * (j + 1)
+
+      if aspect == "landscape":
+        draw.rectangle([left, PALETTE_INNER, right, PALETTE_OUTER], fill=tuple(colorPalette[j][0]), outline=None)
+      else:
+        draw.rectangle([PALETTE_INNER, left, PALETTE_OUTER, right], fill=tuple(colorPalette[j][0]), outline=None)
+
+    # get modified date of original photo 
+    # this allows the easy sorting of the output files by age of the original
     mtime = os.path.getmtime(os.getcwd() + '/input/' + i)
     str_mtime = datetime.datetime.isoformat(datetime.datetime.fromtimestamp(mtime))
     str_mtime = re.sub("[^0-9.]", "-", str_mtime)
